7394192ff493429752aec0821ea2b6510e933617
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2021-09-05 16:14:38 -0300

    use geolocation on map first render

--- code/ts/src/map/mapComponent.tsx
+++ code/ts/src/map/mapComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import styled from 'styled-components'
 
@@ -13,14 +13,66 @@ const DefaultIcon = L.icon({
 })
 
 export function MapComponent(): JSX.Element {
+    const [mapMethods, setMapMethods] = useState<Map<unknown, unknown> | null>(null)
+    const [location, setLocation] = useState<{
+        loaded: boolean
+        coordinates?: { latitude: number; longitude: number }
+        error?: { code: number; message: string }
+    }>({
+        loaded: false,
+        coordinates: { latitude: -19.919946017081916, longitude: -43.995658850819936 }
+    })
+
+    const onSuccess = (location: { coords: { latitude: number; longitude: number } }): void => {
+        setLocation({
+            loaded: true,
+            coordinates: location.coords
+        })
+    }
+
+    const onError = (error: { code: number; message: string }): void => {
+        setLocation({
+            loaded: true,
+            error: {
+                code: error.code,
+                message: error.message
+            }
+        })
+    }
+
+    useEffect(() => {
+        if (!('geolocation' in navigator)) {
+            onError({
+                code: 0,
+                message: 'Geolocation not supported'
+            })
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    }, [])
+    useEffect(() => {
+        console.log(`i ran`)
+        // @ts-ignore
+        if (mapMethods) mapMethods.flyTo([location.coordinates?.latitude || 0, location.coordinates?.longitude || 0])
+    }, [location])
+
     return (
         <StyledMap>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer
+                center={[location.coordinates?.latitude || 0, location.coordinates?.longitude || 0]}
+                zoom={13}
+                scrollWheelZoom={false}
+                zoomAnimation={true}
+                whenCreated={map => setMapMethods(map as any)}
+            >
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]} icon={DefaultIcon}>
+                <Marker
+                    position={[location.coordinates?.latitude || 0, location.coordinates?.longitude || 0]}
+                    icon={DefaultIcon}
+                >
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
