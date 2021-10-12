5db3597064177e6af8092b4cce4026134c606dc4
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2021-10-12 19:55:21 -0300

    extract API_PATHS

--- code/ts/src/apis.ts
+++ code/ts/src/apis.ts
@@ -1,30 +1,37 @@
+const API_PATHS = {
+    USERS: 'USERS',
+    PLACES: 'PLACES',
+    EVENTS: 'EVENTS',
+    AUTH: 'AUTH'
+}
+
 export function postUser(params: UserPostParams): Promise<Response> {
     return new Promise(resolve => {
-        const currentUsers = JSON.parse(window.localStorage.getItem('USERS') || '[]')
-        window.localStorage.setItem('USERS', JSON.stringify([...currentUsers, params]))
+        const currentUsers = JSON.parse(window.localStorage.getItem(API_PATHS.USERS) || '[]')
+        window.localStorage.setItem(API_PATHS.USERS, JSON.stringify([...currentUsers, params]))
         // @ts-ignore
         resolve({ status: 200 })
     })
 }
 export function postCollectionPlace(params: CollecgtionPlacePostParams): Promise<Response> {
     return new Promise(resolve => {
-        const currentUsers = JSON.parse(window.localStorage.getItem('PLACES') || '[]')
-        window.localStorage.setItem('PLACES', JSON.stringify([...currentUsers, params]))
+        const currentUsers = JSON.parse(window.localStorage.getItem(API_PATHS.PLACES) || '[]')
+        window.localStorage.setItem(API_PATHS.PLACES, JSON.stringify([...currentUsers, params]))
         // @ts-ignore
         resolve({ status: 200 })
     })
 }
 export function postEvent(params: EventForm): Promise<Response> {
     return new Promise(resolve => {
-        const currentUsers = JSON.parse(window.localStorage.getItem('EVENTS') || '[]')
-        window.localStorage.setItem('EVENTS', JSON.stringify([...currentUsers, params]))
+        const currentUsers = JSON.parse(window.localStorage.getItem(API_PATHS.EVENTS) || '[]')
+        window.localStorage.setItem(API_PATHS.EVENTS, JSON.stringify([...currentUsers, params]))
         // @ts-ignore
         resolve({ status: 200 })
     })
 }
 export function loginUser(params: LogInParams): Promise<Response> {
     return new Promise(resolve => {
-        const currentUsers = JSON.parse(window.localStorage.getItem('USERS') || '[]')
+        const currentUsers = JSON.parse(window.localStorage.getItem(API_PATHS.USERS) || '[]')
         const existingUser = currentUsers.filter(
             (user: { email: string; password: string }) =>
                 user.email === params.email && user.password === params.password
