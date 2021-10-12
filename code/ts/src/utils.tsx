a18d65cd6d7f93baf902a013b06c0bb549d2d272
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2021-10-12 19:36:08 -0300

    make week days and item types consts

--- code/ts/src/utils.tsx
+++ code/ts/src/utils.tsx
@@ -2,5 +2,9 @@ export const ROUTES = {
     LOGIN: `/login`,
     SIGNIN: `/signIn`,
     NEW_COLLECTION: `/newCollection`,
-    NEW_EVENT: `/newEvent`
+    NEW_EVENT: `/newEvent`,
+    COLLECTION_LIST: `/collectionList`
 }
+
+export const WEEK_DAYS = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', `sábado`, 'domingo'] as const
+export const ITEM_TYPES = ['roupas', 'alimentos perecíveis', 'refeições prontas', 'brinquedos'] as const
