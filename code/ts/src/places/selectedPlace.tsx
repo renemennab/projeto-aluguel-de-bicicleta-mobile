226cdc490fe15227efe61e5623a5dbfb0d75e8d8
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2021-10-24 11:40:45 -0300

    add whatsapp to selected place

--- code/ts/src/places/selectedPlace.tsx
+++ code/ts/src/places/selectedPlace.tsx
@@ -7,6 +7,14 @@ import { AssetActions } from './assetActions'
 export function SelectedPlace(): JSX.Element {
     const { selectedPlace } = useContext(SelectedPlaceContext)
 
+    function handleMessageClick(): void {
+        const phone = `55${selectedPlace?.phone}`
+
+        const url = `https://wa.me/${phone}`
+
+        window.open(url, '_blank')
+    }
+
     return selectedPlace ? (
         <StyledSelectedPlace className={`selectedPlace`}>
             <PageHeader pageName={selectedPlace.name} />
@@ -21,6 +29,11 @@ export function SelectedPlace(): JSX.Element {
             <span>{selectedPlace.workingDays.join(`, `)}</span>
             <span>{selectedPlace.workingHours.to}</span>
             <span>{selectedPlace.workingHours.from}</span>
+
+            <button type="button" className="selectedPlace--message" onClick={handleMessageClick}>
+                Enviar Mensagem
+                <i className="fab fa-whatsapp"></i>
+            </button>
         </StyledSelectedPlace>
     ) : (
         <div />
@@ -32,4 +45,21 @@ const StyledSelectedPlace = styled.div`
     display: flex;
     flex-direction: column;
     position: relative;
+    .selectedPlace {
+        &--message {
+            color: white;
+            border: none;
+            background: #25d366;
+            padding: 15px;
+            border-radius: 8px;
+            font-size: 16px;
+            font-weight: bold;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            i {
+                margin-left: 8px;
+            }
+        }
+    }
 `
