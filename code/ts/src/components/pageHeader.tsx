0ca5ba8cab6abc04c6d17579d8cf406749469933
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2021-11-20 15:00:53 -0300

    fix go back button

--- code/ts/src/components/pageHeader.tsx
+++ code/ts/src/components/pageHeader.tsx
@@ -9,9 +9,17 @@ interface IProps {
 export function PageHeader({ pageName }: IProps): JSX.Element {
     const history = useHistory()
 
+    function handleGoBack(): void {
+        const currentUrl = history.location.pathname
+        const splitUrl = currentUrl.split('/')
+        splitUrl.pop()
+        const newUrl = splitUrl.join('/')
+        history.push(newUrl)
+    }
+
     return (
         <StyledBackArrow>
-            <button type="button" onClick={() => history.goBack()}>
+            <button type="button" onClick={handleGoBack}>
                 <i className={`fa fa-arrow-left`} />
             </button>
             <h1>{pageName}</h1>
