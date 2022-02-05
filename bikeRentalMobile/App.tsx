b529dcffdf819fdcf17b98cd28806ca49ae842dd
Renê Boaventura <reneboaventuraneto@gmail.com>
2022-02-05 16:36:44 -0300

    add global styles

--- bikeRentalMobile/App.tsx
+++ bikeRentalMobile/App.tsx
@@ -1,22 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { createGlobalStyle } from "styled-components";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --yellow: #fccb74;
+
+  }
+`;
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
   }
+  return (
+    <SafeAreaProvider>
+      <GlobalStyle />
+      <Navigation colorScheme={colorScheme} />
+      <StatusBar />
+    </SafeAreaProvider>
+  );
 }
