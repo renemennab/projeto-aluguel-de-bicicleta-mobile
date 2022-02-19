3497d8d67d345d20926820d37cf2dbb18676b38f
Renê Boaventura <reneboaventuraneto@gmail.com>
2022-02-19 19:49:39 -0300

    use scroll view and stylesheet in home

--- bikeRentalMobile/screens/home/homeScreen.tsx
+++ bikeRentalMobile/screens/home/homeScreen.tsx
@@ -1,58 +1,65 @@
-import { Text } from "react-native";
-import styled from "styled-components/native";
+import { ScrollView, Text, SafeAreaView, StyleSheet, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import Lamp from "./lamp";
 import Logo from "./logo";
-// import { getLoggedInUser } from "../common/utils";
-// import OptionsList from "./optionsList";
+import { getLoggedInUser } from "../../common/utils";
+import OptionsList from "./optionsList";
 
 function HomePage(): JSX.Element {
-  //   const isUserLoggedIn = getLoggedInUser();
+  const isUserLoggedIn = getLoggedInUser();
 
   return (
-    <HomeScreenContainer>
-      <Banner>
-        <Logo />
-      </Banner>
-      <LampContainer>
-        <Lamp />
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
-      </LampContainer>
-      <TimeContainer>
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
-        <MaterialIcons name="attach-money" size={84} color="black" />
-      </TimeContainer>
-    </HomeScreenContainer>
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={styles.HomeScreenContainer}>
+        {isUserLoggedIn ? null : (
+          <>
+            <View style={styles.Banner}>
+              <Logo />
+            </View>
+            <View style={styles.LampContainer}>
+              <Lamp />
+              <Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Text>
+            </View>
+            <View style={styles.LampContainer}>
+              <Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Text>
+              <MaterialIcons name="attach-money" size={84} color="black" />
+            </View>
+          </>
+        )}
+        <OptionsList />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 export default HomePage;
-const HomeScreenContainer = styled.View`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-bottom: 40px;
-  width: 100%;
-  height: 100%;
-  background-color: white;
-`;
-
-const Banner = styled.View`
-  width: 100%;
-  height: 50%;
-  min-height: 50%;
-  background-color: #f7d08a;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const LampContainer = styled.View`
-  width: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  margin-top: 20px;
-`;
-const TimeContainer = styled(LampContainer)``;
+const styles = StyleSheet.create({
+  safeArea: { flex: 1 },
+  HomeScreenContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  Banner: {
+    width: "100%",
+    height: 400,
+    backgroundColor: "#f7d08a",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  LampContainer: {
+    width: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: 20,
+  },
+});
