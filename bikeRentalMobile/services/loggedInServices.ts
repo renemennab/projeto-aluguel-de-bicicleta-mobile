ee7ad592cc40eced26372f4e9286b6becaa7df5e
Renê Boaventura <reneboaventuraneto@gmail.com>
2022-03-05 18:03:38 -0300

    parse and stringify user result in login logout funcs

--- bikeRentalMobile/services/loggedInServices.ts
+++ bikeRentalMobile/services/loggedInServices.ts
@@ -6,7 +6,9 @@ const DB_EXEC = Database.getConnection();
 
 export async function getLoggedInUser(): Promise<IlocalStorageProfile> {
   const results = await DB_EXEC(`select * from ${SESSION_DATA.PROFILE}`);
-  return results.rows._array[0];
+  const userObject = results.rows._array[0];
+  if (userObject?.result) userObject.result = JSON.parse(userObject?.result);
+  return userObject;
 }
 
 export const setLoggedInUser = async (param: IlocalStorageProfile | null) => {
@@ -18,7 +20,7 @@ export const setLoggedInUser = async (param: IlocalStorageProfile | null) => {
     results = await DB_EXEC(
       `insert into ${SESSION_DATA.PROFILE} (${SESSION_DATA.RESULT}, ${SESSION_DATA.TOKEN})
         values (?,?)`,
-      [param.result, param.token]
+      [JSON.stringify(param.result), param.token]
     );
   } catch (error) {
     console.log("setLoggedInUser error", error);
@@ -38,7 +40,7 @@ interface IDecodedToken {
 
 export async function checkIfTokenIsExpired(): Promise<boolean> {
   const user = await getLoggedInUser();
-  if (!user.token) return true;
+  if (!user?.token) return true;
 
   const decodedToken = jwtDecode<IDecodedToken>(user.token);
   const timeNowInMilisseconds = new Date().getTime();
