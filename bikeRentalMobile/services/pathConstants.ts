6b23dd8183e1d5f0611e6e9d2cb969849c5c7abf
Renê Boaventura <reneboaventuraneto@gmail.com>
2022-04-09 17:10:20 -0300

    make path constants type const

--- bikeRentalMobile/services/pathConstants.ts
+++ bikeRentalMobile/services/pathConstants.ts
@@ -2,8 +2,9 @@ export default {
   BIKES: "bikes",
   RESERVATIONS: "reservations",
   USER: "user",
+  USERS: "users",
   LOGIN: "login",
   SIGNUP: "signup",
   BIKES_BY_DATES: "bikesByDates",
   RATING: "rating",
-};
+} as const;
